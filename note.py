055770154c2d126418d6ad571049f3defeb20284
Jun3-Alex <130096011+Jun3-Alex@users.noreply.github.com>
2023-12-19 22:55:36 +0500

    update-cls-note

--- note.py
+++ note.py
@@ -1,14 +1,38 @@
-import datetime
+from datetime import datetime
+import secrets
 
 
 class Note:
-    def __init__(self, title: str, content: str, date: datetime, priority: str):
+    def __init__(self, title: str, body: str, note_id=None, last_modified=None):
+        self.note_id = note_id if note_id else self.generate_id()
         self.title = title
-        self.content = content
-        self.date = date
-        self.priority = priority
+        self.body = body
+        self.last_modified = last_modified if last_modified else datetime.now()
 
-    def __str__(self):
-        return (f'{self.title}<{self.priority}>:\n'
-                f'{self.content}\n'
-                f'{self.date}')
+    @staticmethod
+    def generate_id():
+        return secrets.token_hex(4)
+
+    def update(self, title, body):
+        self.title = title
+        self.body = body
+        self.last_modified = datetime.now()
+
+    def to_dict(self):
+        return {
+            "id": self.note_id,
+            "title": self.title,
+            "body": self.body,
+            "last_modified": self.last_modified.strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data['title'],
+            data['body'],
+            data['id'],
+            datetime.strptime(data['last_modified'], '%Y-%m-%d %H:%M:%S')
+        )
+
+
